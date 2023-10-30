b09f66585dff7dcb7195572606c454343d01cb4c
Manish <manishsuresh993@gmail.com>
2023-10-30 08:46:01 +0530

    A simple website for now

--- app/layout.js
+++ app/layout.js
@@ -1,8 +1,10 @@
-import Cursor from "@/components/common/cursor";
 import "./globals.css";
-import { Inter } from "next/font/google";
+import { Cormorant_Garamond } from "next/font/google";
 
-const inter = Inter({ subsets: ["latin"] });
+const corm = Cormorant_Garamond({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "300", "700"],
+});
 
 export const metadata = {
   title: "bLanK",
@@ -11,13 +13,8 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html
-      lang="en"
-      className="bg-black text-slate-950 max-w-screen overflow-hidden  select-none">
-      <body className={inter.className}>
-        {" "}
-        <Cursor /> {children}
-      </body>
+    <html lang="en" className="max-w-screen overflow-hidden">
+      <body className={corm.className}>{children}</body>
     </html>
   );
 }
